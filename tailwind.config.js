de12710540aec0bad1f807e4b5fed2cac81f8575
Mesrop <mesropmkl@gmail.com>
2025-05-19 11:00:42 +0200

    Initialize react native & tailwind css style

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#60D7FF",
+        neutral: {
+          0: "#FFFFFF",
+          50: "#FBFBFB",
+          100: "#D4D4D4",
+          200: "#B6B6B6",
+          300: "#878787",
+          400: "#484848",
+          450: "#292929",
+          500: "#1B1B1B",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
