358b380e594d52f0114a9f744730e25070c7571e
BERTIN Ian <ian.bertin2004@gmail.com>
2025-05-19 13:39:42 +0200

    feat(#3): Initialisation de react intl - Closes #3

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,41 @@
+import { IntlProvider, useIntl } from 'react-intl';
 import { Text, View } from "react-native";
+import { langs } from "./interfaces/lang.js";
+import { en } from "./lang/en";
+import { fr } from "./lang/fr";
 
-export default function Index() {
+const Test = () => {
+  const intl = useIntl()
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-5xl text-blue-600">Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>{intl.formatMessage({id:"test"})}</Text>
     </View>
+  )
+}
+
+const Index = () => {
+  const messages : langs = {
+    en,
+    fr,
+  };
+   
+  const language = navigator.language.split('-')[0];
+  const locale = Object.keys(messages).includes(language) ? language : 'en';
+
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text className="text-5xl text-blue-600">{<Test/>}</Text>
+      </View>
+    </IntlProvider>
   );
 }
+
+export default Index
