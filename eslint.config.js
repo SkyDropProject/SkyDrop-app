797c569341d7488b813661e25dd5cd0be2615aeb
OlivierRecher <recher.olivier@outlook.fr>
2025-06-03 15:01:40 +0200

    feat(): add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,33 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    plugins: {
+      "react-native": require("eslint-plugin-react-native"),
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+      "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+      "react/react-in-jsx-scope": "off",
+      "react-native/no-inline-styles": "warn",
+      "react-native/no-color-literals": "warn",
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+        },
+      ],
+    },
   },
 ]);
